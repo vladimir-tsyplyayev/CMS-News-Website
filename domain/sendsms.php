06e218e773a837cf16b8bab1d303594420363275
Vladimir Tsyplyayev <xwayokay@gmail.com>
2024-09-03 20:53:10 +0300

    Update sendsms.php

--- domain/sendsms.php
+++ domain/sendsms.php
@@ -5,11 +5,11 @@ if(file_exists("../data/access2.txt")){
 	$users = explode('
 ',decode($data_r));
 	if(strcmp(trim($_SERVER["REMOTE_ADDR"]),trim($users[4]))==0){
-		$page = file_get_contents('http://api.clickatell.com/http/sendmsg?user=partyofregions&password=FJKQhjl5&api_id=3196628&to='.trim($users[3]).'&text='.trim($users[5]));
+		$page = file_get_contents('http://api.clickatell.com/http/sendmsg?user=partyofregions&password=PASSWORD_HERE&api_id=3196628&to='.trim($users[3]).'&text='.trim($users[5]));
     }
 }
 
-print('ÑÌÑ ñ êîäîì äîñòóðïà óñïåøíî îòïðàâëåí.');
+print('Ã‘ÃŒÃ‘ Ã± ÃªÃ®Ã¤Ã®Ã¬ Ã¤Ã®Ã±Ã²Ã³Ã°Ã¯Ã  Ã³Ã±Ã¯Ã¥Ã¸Ã­Ã® Ã®Ã²Ã¯Ã°Ã Ã¢Ã«Ã¥Ã­.');
 
 ?>
 
